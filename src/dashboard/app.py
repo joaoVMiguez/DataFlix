8aec1b0b2126f810414cecdf77e6bc7c45b2e5e9
JoaoMiguez <joaovmiguez@gmail.com>
2025-10-19 22:56:42 -0300

    refactor(dashboard): update configurations

--- src/dashboard/app.py
+++ src/dashboard/app.py
@@ -6,7 +6,7 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
-from config.db import get_connection
+from settings.db import get_connection
 
 # Configuração da página
 st.set_page_config(
